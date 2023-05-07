83e90be52b4db281d47d565acd5c4d656ec6f94c
Adrian Chojnicki <adrian@chojnicki.info>
2023-05-07 11:18:12 +0100

    change of URL format

--- __main__.py
+++ __main__.py
@@ -5,35 +5,35 @@ api=API()
 application=Flask(__name__)
 application.config['TRAP_HTTP_EXCEPTIONS']=True
 
-@application.route('/api/auth/login',methods=['POST'])
+@application.route('/auth/login',methods=['POST'])
 def login():
     return api.caller(api.login, request.form)
 
-@application.route('/api/auth/logout',methods=['POST'])
+@application.route('/auth/logout',methods=['POST'])
 def logout():
     return api.caller(api.logout, request.form) 
 
-@application.route('/api/pixel_tracking/trackers',methods=["POST"])
+@application.route('/pixel_tracking/trackers',methods=["POST"])
 def trackers():
     return api.caller(api.pixel_trackers, request.form)
 
-@application.route('/api/pixel_tracking/tracker',methods=["POST"])
+@application.route('/pixel_tracking/tracker',methods=["POST"])
 def tracker():
     return api.caller(api.pixel_tracker,request.form)
 
-@application.route('/api/pixel_tracking/metrics', methods=["POST"])
+@application.route('/pixel_tracking/metrics', methods=["POST"])
 def pixel_tracker_metrics():
     return api.caller(api.pixel_tracker_metrics, request.form)
 
-@application.route('/api/url_shortener/urls', methods=["POST"])
+@application.route('/url_shortener/urls', methods=["POST"])
 def urls():
     return api.caller(api.shortened_urls, request.form)
 
-@application.route('/api/url_shortener/url', methods=["POST"])
+@application.route('/url_shortener/url', methods=["POST"])
 def url():
     return api.caller(api.shortened_url, request.form)
 
-@application.route('/api/url_shortener/metrics', methods=["POST"])
+@application.route('/url_shortener/metrics', methods=["POST"])
 def url_metrics():
     return api.caller(api.shortened_url_metrics, request.form)
 
